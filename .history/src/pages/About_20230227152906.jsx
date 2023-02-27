70d75ac98c639104af01f1507cf4fc36c3b360f5
falkon2 <94387470+falkon2@users.noreply.github.com>
2023-02-27 15:32:08 +0530

    background and text change

--- .history/src/pages/About_20230227152906.jsx
+++ .history/src/pages/About_20230227152906.jsx
@@ -22,7 +22,7 @@ export const About = () => {
           </div>
           <p className="md:text-2xl sm:text-xl text-lg font-bold py-4 ">
           Hello and thank you for visiting Web Geny, Here you can easily construct your own website that may be entirely personalised and is also completely free! This website builder is focused on <a className="underline text-indigo-600">design</a> and <a className="underline text-indigo-600"> simplicity</a>.</p>
-          <p className='md:text-xl sm:text-lg text-base'> This website builder was made by <a href='https://github.com/falkon2' className="underline text-indigo-300 ">Falkon</a></p>
+          <p className='md:text-xl sm:text-lg text-base'> This website builder was made by <a href='https://github.com/falkon2' className="underline text-indigo-300 after:transition-[0.5s] after:ease-out hover:after:scale-x-100 hover:scale-110">Falkon</a></p>
           <Link to="/"><button className="bg-white text-black w-[200px] transition-[0.5s] rounded-lg font-bold my-6 mx-auto py-3 ring-2 ring-white hover:ring-indigo-600 hover:bg-indigo-600  hover:shadow-xl hover:shadow-indigo-700 hover:text-white hover:scale-110">
             Home
           </button></Link>
